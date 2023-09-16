a3f2d2a51a259ef3dc7a648d2a16d619e3c4fc8c
Aimé Risson <aime.risson.1@gmail.com>
2023-09-16 16:36:27 +0200

    export pointages (#41)

--- apps/api/api/pointage/urls.py
+++ apps/api/api/pointage/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from api.pointage.view import PointageList, PointageGestion
+from api.pointage.view import PointageList, PointageGestion, ExportPointagesView
 
 urlpatterns = [
     path("list", PointageList.as_view()),
     path("", PointageGestion.as_view()),
+    path("export", ExportPointagesView.as_view()),
 ]
