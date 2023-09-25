5d1d43083d327f40202243474a320f0171acc88d
Risson <aime.risson.1@gmail.com>
2023-09-25 17:48:40 +0200

    tests

--- apps/api/api/tests/test_affectations.py
+++ apps/api/api/tests/test_affectations.py
@@ -2,13 +2,14 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 
 from api.affaire.models import Affaire
-from api.affectation.models import AffectationMachine
+from api.affectation.models import AffectationMachine, AffectationAjustage
 from api.etape.models import Etape
 from api.fiche.models import Fiche
 from api.groupe_machine.models import GroupeMachine
 from django.utils import timezone
 
 from api.machine.models import Machine
+from api.zone.models import Zone
 
 
 class AffectationMachineTest(APITestCase):
@@ -56,7 +57,6 @@ class AffectationMachineTest(APITestCase):
             "semaine_affectation": timezone.now().strftime("%Y-%m-%d"),
         }
         response = self.client.post(url, data, format="json")
-        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_create_affectation_machine_etape_deja_affectee(self):
@@ -150,33 +150,106 @@ class AffectationMachineTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 
-class AffectationAjustage(APITestCase):
+class AffectationAjustageTest(APITestCase):
+    def setUp(self):
+        self.groupe_machine_ajustage = GroupeMachine.objects.create(
+            nom_groupe="Ajustage", prix_theorique=100
+        )
+
+        self.zone = Zone.objects.create(nom="Zone test", description="Description test")
+
+        self.affaire = Affaire.objects.create(num_affaire=1)
+
+        self.fiche = Fiche.objects.create(
+            titre="Fiche test",
+            affaire=self.affaire,
+            fourniture=False,
+            id=1,
+        )
+
+        self.etape1 = Etape.objects.create(
+            fiche=self.fiche, groupe_machine=self.groupe_machine_ajustage, num_etape=1
+        )
+
+        self.etape2 = Etape.objects.create(
+            fiche=self.fiche,
+            groupe_machine=self.groupe_machine_ajustage,
+            num_etape=1,
+            terminee=True,
+        )
+
     def test_create_affectation_ajustage(self):
         """
         Teste la création d'une affectation ajustage
         """
-        pass
+
+        url = "/api/affectations/ajustage"
+        data = {
+            "etape": self.etape1.id,
+            "semaine_affectation": timezone.now().strftime("%Y-%m-%d"),
+            "zone": self.zone.id,
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_list_affectation_ajustage(self):
         """
         Teste la récupération de toutes les affectations ajustage
         """
-        pass
+        AffectationAjustage.objects.create(
+            etape=self.etape1,
+            semaine_affectation=timezone.now().strftime("%Y-%m-%d"),
+            zone=self.zone,
+        )
+
+        url = "/api/affectations/ajustage"
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.json()["results"]), 1)
 
     def test_retrieve_affectation_ajustage(self):
         """
         Teste la récupération d'une affectation ajustage
         """
-        pass
+        affectation = AffectationAjustage.objects.create(
+            etape=self.etape1,
+            semaine_affectation=timezone.now().strftime("%Y-%m-%d"),
+            zone=self.zone,
+        )
+
+        url = f"/api/affectations/ajustage/{affectation.id}"
+        response = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_affectation_ajustage(self):
         """
         Teste la mise à jour d'une affectation ajustage
         """
-        pass
+        affectation = AffectationAjustage.objects.create(
+            etape=self.etape1,
+            semaine_affectation=timezone.now().strftime("%Y-%m-%d"),
+            zone=self.zone,
+        )
+
+        url = f"/api/affectations/ajustage/{affectation.id}"
+        data = {
+            "semaine_affectation": (
+                timezone.now() + timezone.timedelta(days=7)
+            ).strftime("%Y-%m-%d")
+        }
+        response = self.client.patch(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_delete_affectation_ajustage(self):
         """
         Teste la suppression d'une affectation ajustage
         """
-        pass
+        affectation = AffectationAjustage.objects.create(
+            etape=self.etape1,
+            semaine_affectation=timezone.now().strftime("%Y-%m-%d"),
+            zone=self.zone,
+        )
+
+        url = f"/api/affectations/ajustage/{affectation.id}"
+        response = self.client.delete(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
