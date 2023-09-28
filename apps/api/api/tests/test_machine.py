85d5bb9942c9fd8acc437c3a225b1cb9b06184f3
Risson <aime.risson.1@gmail.com>
2023-09-28 15:53:57 +0200

    test machine

---
+++ apps/api/api/tests/test_machine.py
@@ -0,0 +1,60 @@
+from rest_framework.test import APITestCase
+
+from api.groupe_machine.models import GroupeMachine
+from api.machine.models import Machine
+
+
+class MachineTest(APITestCase):
+    def setUp(self):
+        self.group1 = GroupeMachine.objects.create(nom_groupe="group1")
+        self.machine1 = Machine.objects.create(
+            nom_machine="machine1",
+            groupe_machine=self.group1,
+        )
+        self.machine2 = Machine.objects.create(
+            nom_machine="machine2",
+            groupe_machine=self.group1,
+        )
+
+    def test_list(self):
+        response = self.client.get("/api/machines/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data["results"]), 2)
+
+    def test_create(self):
+        response = self.client.post(
+            "/api/machines/",
+            {
+                "nom_machine": "machine3",
+                "groupe_machine": self.group1.id,
+            },
+        )
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Machine.objects.count(), 3)
+
+    def test_retrieve(self):
+        response = self.client.get(f"/api/machines/{self.machine1.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["nom_machine"], "machine1")
+
+    def test_update(self):
+        response = self.client.patch(
+            f"/api/machines/{self.machine1.id}",
+            {
+                "nom_machine": "machine1_updated",
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["nom_machine"], "machine1_updated")
+
+    def test_bulk_delete(self):
+        response = self.client.post(
+            "/api/machines/delete",
+            {
+                "ids": [self.machine2.id, self.machine1.id],
+            },
+            format="json",
+        )
+        print(Machine.objects.all())
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Machine.objects.count(), 0)
