fb14b8e1a3747de01ae5b2e4bcd5aa56769ae3c2
Risson <aime.risson.1@gmail.com>
2023-09-28 13:20:28 +0200

    zones CRUD

--- apps/api/api/zone/urls.py
+++ apps/api/api/zone/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from api.zone.view import ZoneList
+from api.zone.view import ZoneListCreate, ZoneRUD, ZonesBulkDelete
 
-urlpatterns = [path("", ZoneList.as_view())]
+urlpatterns = [
+    path("", ZoneListCreate.as_view()),
+    path("delete", ZonesBulkDelete.as_view()),
+    path("<int:pk>", ZoneRUD.as_view()),
+]
--- apps/api/api/zone/view.py
+++ apps/api/api/zone/view.py
@@ -1,11 +1,26 @@
 from drf_spectacular.utils import extend_schema
 
+from api.commun.views import BulkDeleteView
 from api.zone.models import Zone
 from api.zone.serializer import ListZoneSerializer
 from rest_framework import generics
 
 
-@extend_schema(tags=["Zone"], description="Lister les zones")
-class ZoneList(generics.ListAPIView):
+@extend_schema(tags=["Zone"], description="Créer / Lister une zone")
+class ZoneListCreate(generics.ListCreateAPIView):
+    queryset = Zone.objects.all().order_by("nom")
+    serializer_class = ListZoneSerializer
+
+
+@extend_schema(tags=["Zone"], description="Récupérer et Updater  une zone")
+class ZoneRUD(generics.RetrieveUpdateAPIView):
     queryset = Zone.objects.all()
     serializer_class = ListZoneSerializer
+
+
+@extend_schema(
+    summary="Bulk delete de Zone",
+    tags=["Zone"],
+)
+class ZonesBulkDelete(BulkDeleteView):
+    queryset = Zone.objects.all()
