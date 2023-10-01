a7b4a91301772ba4269eb4eb09ad86a2344fffde
Risson <aime.risson.1@gmail.com>
2023-10-01 11:03:00 +0200

    remove

--- apps/api/api/utils/dates.py
+++ apps/api/api/utils/dates.py
@@ -1,14 +1,6 @@
 from datetime import datetime, timedelta
 
 
-# Merci à chatGPT pour le code et les regles concernant les dates (IsO 8601) :
-# To correctly use the ISO 8601 standard in Python, you should use %G, %V, and %u, where:
-#
-# %G: The ISO 8601 week-numbering year, which is slightly different than the regular calendar year.
-# %V: The ISO 8601 week number.
-# %u: The ISO 8601 weekday, where Monday is 1 and Sunday is 7.
-
-
 def week_to_date_range(date_str) -> (datetime, datetime):
     """
     Convertit une semaine en une plage de dates.
