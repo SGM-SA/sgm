40c6284c7814ac13a4c792ffbac7ae8d56e189f2
Risson <aime.risson.1@gmail.com>
2023-10-21 13:08:25 +0200

    création affectation

--- apps/api/api/affectation/serializer.py
+++ apps/api/api/affectation/serializer.py
@@ -1,8 +1,155 @@
+from typing import Union, Type
+
 from rest_framework import serializers
-from api.affectation.models import AffectationMachine, AffectationAjustage
+from rest_framework.exceptions import ValidationError
+
+from api.affectation.models import AffectationMachine, AffectationAjustage, Affectation
+from api.utils.dates import week_to_date_range
+from datetime import datetime
 
 
-class AffectationMachineSerializer(serializers.ModelSerializer):
+class AffectationAbstractSerializer(serializers.ModelSerializer):
+    """
+    Serializer abstract pour l'affectation d'une machine ou d'un ajustage à une étape, comprenant toute la logique
+    """
+
+    class Meta:
+        model = Affectation
+        fields = "__all__"
+
+    def validate(self, data):
+        instance = self.Meta.model(**data)
+        try:
+            instance.validate_unique()
+        except ValidationError as e:
+            raise serializers.ValidationError(e.detail)
+        return data
+
+    def create(
+        self,
+        validated_data,
+        model_class: Type[Union[AffectationMachine, AffectationAjustage]],
+        filter_field: Union["machine", "zone"],
+    ):
+        """
+        Création d'une affectation. La logique est découpée en semaine.
+
+
+        Attention previous représente l'affectation au-dessus de la nouvelle affectation
+        3 cas sont possibles :
+        - L'affectation est en tête de liste
+        - L'affectation est en queue de liste
+        - L'affectation est entre deux affectations
+
+        :param validated_data: Données validées
+        :param model_class: Classe du modèle à utiliser AffectionMachine ou AffectationAjustage
+        :param filter_field: Champ de filtrage pour la requête
+
+        """
+        # vérifier que la classe est bien une classe d'affectation
+        if not issubclass(model_class, Affectation):
+            raise ValueError("model_class must be a subclass of Affectation")
+
+        previous_field = validated_data.get("previous", None)
+        date_affectation = validated_data.get("semaine_affectation")
+        specific_field = (
+            validated_data.get("machine")
+            if model_class == AffectationMachine
+            else validated_data.get("zone")
+        )
+
+        # si le champ 'previous' est null, on veut insérer en tête de liste
+        if previous_field is None:
+            head = model_class.objects.filter(
+                previous__isnull=True,
+                semaine_affectation__range=week_to_date_range(
+                    date_affectation.strftime("%Y-%m-%d")
+                ),
+                **{filter_field: specific_field},  # on filtre par machine ou zone
+            ).first()
+
+            current = model_class.objects.create(**validated_data)
+
+            # si la liste n'est pas vide, on insère en tête de liste et on met à jour l'objet head
+            if head is not None:
+                head.previous = current
+                head.save()
+
+            return current
+
+        else:
+            print("ici")
+            # on récupère l'objet affectation précédent
+            previous = previous_field
+
+            print(previous.id)
+
+            # on vérifie qu'elle est bien dans la même range de date (week_to_date_range)
+            if week_to_date_range(
+                previous.semaine_affectation.strftime("%Y-%m-%d")
+            ) != week_to_date_range(date_affectation.strftime("%Y-%m-%d")):
+                raise serializers.ValidationError(
+                    "L'affectation précédente n'est pas dans la même semaine"
+                )
+
+            # on récupère l'objet affectation suivant
+            next = previous.next.first()
+
+            # on crée l'objet courant, après avoir récupéré next (sinon on a un faux next)
+            current = model_class.objects.create(**validated_data)
+
+            # si l'objet suivant n'est pas null, on insère entre les deux
+            if next is not None:
+                next.previous = current
+                next.save()
+
+            return current
+
+    def update(self, instance, validated_data):
+        """
+        Mise à jour d'une affectation. La logique est découpée en semaine.
+
+        Dans le cas d'une affectation, on ne met à jour que le fiedl previous ou la machine / zone
+
+        3 cas sont possibles :
+        - L'affectation est mise en tête de liste
+        - L'affectation est mise en queue de liste
+        - L'affectation est mise entre deux affectations
+
+        :param instance: Instance de l'affectation à mettre à jour
+        :param validated_data: Données validées
+        """
+
+        new_previous = validated_data.get("previous", None)
+
+        # on récupère l'objet affectation précédent
+        previous = instance.previous
+
+        # on récupère l'objet affectation suivant
+        next = instance.next.first()
+
+        # si le champ new_previous est null, on veut insérer en tête de liste
+        if new_previous is None:
+            # si l'objet précédent est null, on est déjà en tête de liste
+            if previous is None:
+                return instance
+
+            # on met à jour le suivant pour qu'il pointe vers l'objet précédent
+            next.previous = previous
+            next.save()
+
+            # on met à jour l'objet courant
+            instance.previous = None
+            instance.save()
+
+            # on met à jour l'objet précédent
+            previous.previous = instance
+            previous.save()
+
+            return instance
+
+
+class AffectationMachineSerializer(AffectationAbstractSerializer):
     """
     Serializer pour l'affectation d'une machine à une étape
     """
@@ -11,8 +158,11 @@ class AffectationMachineSerializer(serializers.ModelSerializer):
         model = AffectationMachine
         fields = "__all__"
 
+    def create(self, validated_data):
+        return super().create(validated_data, AffectationMachine, "machine")
 
-class AffectationAjustageSerializer(serializers.ModelSerializer):
+
+class AffectationAjustageSerializer(AffectationAbstractSerializer):
     """
     Serializer pour l'affectation d'un ajustage à une étape
     """
@@ -20,3 +170,6 @@ class AffectationAjustageSerializer(serializers.ModelSerializer):
     class Meta:
         model = AffectationAjustage
         fields = ["id", "etape", "zone", "semaine_affectation", "previous"]
+
+    def create(self, validated_data):
+        return super().create(validated_data, AffectationAjustage, "zone")
--- apps/api/api/affectation/views.py
+++ apps/api/api/affectation/views.py
@@ -16,56 +16,6 @@ class AffectationMachineCreate(generics.CreateAPIView):
     queryset = AffectationMachine.objects.all()
     serializer_class = AffectationMachineSerializer
 
-    def create(self, request, *args, **kwargs):
-        # affectation uses a previous field wich indicates the previous affectation (order to be handled)
-        # if the revious field is null, we need to search for the affectation wich has null as previous field
-        # and set this affectation as the previous ofthe new affectation
-        serializer: AffectationMachineSerializer = self.get_serializer(
-            data=request.data
-        )
-        serializer.is_valid(raise_exception=True)
-
-        current: AffectationMachine = serializer.create(serializer.validated_data)
-
-        # Récupération du champ 'previous' et 'date_affectation' depuis les données de la requête
-        previous_field = serializer.validated_data.get("previous", None)
-        date_affectation = serializer.validated_data.get("semaine_affectation")
-        machine = serializer.validated_data.get("machine")
-
-        # si le champ 'previous' est null, on veut insérer en tête de liste
-        if previous_field is None:
-            head = (
-                AffectationMachine.objects.filter(
-                    previous__isnull=True,
-                    semaine_affectation=date_affectation,
-                    machine=machine,
-                )
-                .exclude(id=current.id)
-                .first()
-            )
-            # si la liste n'est pas vide, on insère en tête de liste
-            if head is not None:
-                head.previous = current
-                head.save()
-
-        else:
-            # on récupère l'objet affectation précédent
-            previous = AffectationMachine.objects.get(id=previous_field.id)
-
-            # on récupère l'objet affectation suivant
-            next = (
-                AffectationMachine.objects.filter(previous=previous)
-                .exclude(id=current.id)
-                .first()
-            )
-            if next is not None:
-                # on met à jour l'objet affectation précédent
-                next.previous = current
-                next.save()
-
-        serializer = AffectationMachineSerializer(current)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
 
 @extend_schema(
     tags=["Affectation Machine"],
@@ -83,56 +33,6 @@ class AffectationMachineCRUD(generics.RetrieveUpdateDestroyAPIView):
 class AffectationAjustageCreate(generics.CreateAPIView):
     queryset = AffectationAjustage.objects.all()
     serializer_class = AffectationAjustageSerializer
-
-    def create(self, request, *args, **kwargs):
-        # affectation uses a previous field wich indicates the previous affectation (order to be handled)
-        # if the revious field is null, we need to search for the affectation wich has null as previous field
-        # and set this affectation as the previous ofthe new affectation
-        serializer: AffectationAjustageSerializer = self.get_serializer(
-            data=request.data
-        )
-        serializer.is_valid(raise_exception=True)
-
-        current: AffectationAjustage = serializer.create(serializer.validated_data)
-
-        # Récupération du champ 'previous' et 'date_affectation' depuis les données de la requête
-        previous_field = serializer.validated_data.get("previous", None)
-        date_affectation = serializer.validated_data.get("semaine_affectation")
-        zone = serializer.validated_data.get("zone")
-
-        # si le champ 'previous' est null, on veut insérer en tête de liste
-        if previous_field is None:
-            head = (
-                AffectationAjustage.objects.filter(
-                    previous__isnull=True,
-                    semaine_affectation=date_affectation,
-                    zone=zone,
-                )
-                .exclude(id=current.id)
-                .first()
-            )
-            # si la liste n'est pas vide, on insère en tête de liste
-            if head is not None:
-                head.previous = current
-                head.save()
-
-        else:
-            # on récupère l'objet affectation précédent
-            previous = AffectationAjustage.objects.get(id=previous_field.id)
-
-            # on récupère l'objet affectation suivant
-            next = (
-                AffectationAjustage.objects.filter(previous=previous)
-                .exclude(id=current.id)
-                .first()
-            )
-            if next is not None:
-                # on met à jour l'objet affectation précédent
-                next.previous = current
-                next.save()
-
-        serializer = AffectationAjustageSerializer(current)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @extend_schema(
