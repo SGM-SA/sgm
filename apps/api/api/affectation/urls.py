9bf039246a56b0880af7121b877250cb4414cf2d
Risson <aime.risson.1@gmail.com>
2023-10-21 23:04:36 +0200

    uniquement vues nécessaires

--- apps/api/api/affectation/urls.py
+++ apps/api/api/affectation/urls.py
@@ -4,12 +4,12 @@ from api.affectation.views import (
     AffectationMachineCreate,
     AffectationMachineUpdateDelete,
     AffectationAjustageCreate,
-    AffectationAjustageCRUD,
+    AffectationAjustageUpdateDelete,
 )
 
 urlpatterns = [
     path("machines", AffectationMachineCreate.as_view()),
     path("machines/<int:pk>", AffectationMachineUpdateDelete.as_view()),
     path("ajustages", AffectationAjustageCreate.as_view()),
-    path("ajustages/<int:pk>", AffectationAjustageCRUD.as_view()),
+    path("ajustages/<int:pk>", AffectationAjustageUpdateDelete.as_view()),
 ]
