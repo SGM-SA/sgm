32ffc4b98ef5e5f9dd7988859668ac417ab55906
Bartholomé Gili <bgili@sqli.com>
2023-10-23 22:41:08 +0200

    feat(web/fiches): print button working (#62)

--- apps/web/src/components/modules/fiche/PrintFicheButton.tsx
+++ apps/web/src/components/modules/fiche/PrintFicheButton.tsx
@@ -1,4 +1,5 @@
 import { Button, ThemeComponentProps } from '@chakra-ui/react'
+import { fetchApiFichesExportRetrieve } from '@sgm/openapi'
 import React from 'react'
 
 type PrintFicheButtonProps = {
@@ -8,16 +9,26 @@ type PrintFicheButtonProps = {
 
 export const PrintFicheButton: React.FC<PrintFicheButtonProps> = (props) => {
 
+  const handleClick = async () => {
+    const res = (await fetchApiFichesExportRetrieve({ queryParams: { fiche_id: props.ficheId }})) as string | undefined
+    if (!res) return
+
+    // cursed technique but hey, it works
+    const url = window.URL.createObjectURL(new Blob([res]))
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', new Date().toISOString()); //
+    document.body.appendChild(link);
+    link.click();
+  }
+
 	return <>
         <Button
             size={props.buttonSize || 'sm'}
             colorScheme='blue'
             borderRadius='4px'
             variant='outline'
-            onClick={() => {
-                // TODO: implement
-                window.print() // temporary
-            }}
+            onClick={handleClick}
         >
             Imprimer
         </Button>
